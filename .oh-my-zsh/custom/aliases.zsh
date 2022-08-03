99b9746f921c32d653db909890c29593fbaa09b7
ymzayek <yasmin.mzayek@inria.fr>
2022-08-03 11:09:39 +0200

    New aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,27 +1,25 @@
-#initialize FreeSurfer
+# Initialize FreeSurfer
 
 alias fs='export FREESURFER_HOME=/usr/local/freesurfer/7.2.0/; source $FREESURFER_HOME/SetUpFreeSurfer.sh'
 
 # Aliases for dev environment
 
 alias nd='conda activate nilearn-dev; cd ~/nilearn/nilearn'
+alias quickdocs='cd ~/nilearn/nilearn/doc; make html-noplot'
 
 # Git aliases
 
 alias cleand='git clean -xdn -e mytests -e .idea/'
 alias clean='git clean -xfd -e mytests -e .idea/'
 alias unstage='git restore --staged'
+alias flakediff='COMMIT=$(git merge-base main @); git diff $COMMIT | flake8 --diff'
+alias cifull='git commit --allow-empty -m "[circle full] request full build"'
+alias gits='git status'
+alias gitc='git checkout'
+alias gitcm='git checkout main'
 
 # Other aliases
 
 alias ssh-drago-nb='ssh -L 9429:localhost:9429 drago -t "/storage/store/work/ymzayek/miniforge/bin/jupyter-notebook --port 9429 --no-browser --NotebookApp.token='' --NotebookApp.iopub_data_rate_limit=1.0e7 --NotebookApp.notebook_dir='/storage/store/work/ymzayek/nilearn-fork/nilearn'"'
-
 alias fnd='history | grep'
-
 alias pych='pycharm.sh&'
-
-alias full='git commit --allow-empty -m "[circle full] request full build"'
-
-alias flakediff='COMMIT=$(git merge-base main @); git diff $COMMIT | flake8 --diff'
-
-alias gis='git status'
